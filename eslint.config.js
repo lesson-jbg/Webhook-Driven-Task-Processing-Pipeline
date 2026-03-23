7148c5d458173afb81cb1b83186ea7ad0bfdbed9
lesson-jbg <salehbissan22@gmail.com>
2026-03-23 23:17:02 +0200

    fix formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,20 @@
-const tsParser = require("@typescript-eslint/parser");
-const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
 
 module.exports = [
   {
-    files: ["**/*.ts"],
-    ignores: ["dist/**", "node_modules/**"],
+    files: ['**/*.ts'],
+    ignores: ['dist/**', 'node_modules/**'],
     languageOptions: {
       parser: tsParser,
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
     plugins: {
-      "@typescript-eslint": tsPlugin,
+      '@typescript-eslint': tsPlugin,
     },
     rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
 ];
